919130202983bdd1488d7df973e4162ad447ed6d
Cristian1997Ion <cristian1997ion@yahoo.com>
2020-10-09 20:59:04 +0300

    migration: added create_clients_table migration

---
+++ database/migrations/2020_10_09_171754_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateClientsTable
+ */
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // We use a default_avatar.jpg if the user doesn't have an avatar
+
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email',128);
+            $table->boolean('has_avatar')->default(false);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
